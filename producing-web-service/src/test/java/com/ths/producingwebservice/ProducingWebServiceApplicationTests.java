e102c35213d5583b5c9d127030511427c56e7bdc
triyasmkom <triyavian@gmail.com>
2022-08-17 22:13:15 +0700

    Producing a SOAP web service

---
+++ producing-web-service/src/test/java/com/ths/producingwebservice/ProducingWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ths.producingwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducingWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
